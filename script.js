fa2ba0327838f93eca0d2587f74985a7dc0ef60a
mijade <145517668+mijade@users.noreply.github.com>
2023-10-16 14:18:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-d
+const body = document.body;
+const toggleButton = document.getElementById('toggleButton');
+
+toggleButton.addEventListener('change', function() {
+    if (this.checked) {
+        body.style.backgroundImage = 'url("neon1.gif")'; // Set your alternate image path
+    } else {
+        body.style.backgroundImage = 'url("neon.gif")'; // Set your default image path
+    }
+});
